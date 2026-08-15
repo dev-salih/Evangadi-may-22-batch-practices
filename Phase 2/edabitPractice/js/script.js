beeb0d579da620c7890104d17ba54f203642b104
dev-salih <salihmu79@gmail.com>
2026-08-15 18:06:31 +0300

    edabit Q-4 & Q-5 are included

--- Phase 2/edabitPractice/js/script.js
+++ Phase 2/edabitPractice/js/script.js
@@ -112,3 +112,39 @@ console.log(emotify("Make me smile"));
 // console.log(emotify("Make me grin"));
 // console.log(emotify("Make me sad"));
 // console.log(emotify("Make me mad"));
+
+// Q-3
+// A decimal number can be represented as a sequence of bits. To illustrate:
+
+// 6 = 00000110
+// 23 = 00010111
+// From the bitwise representation of numbers, we can calculate the bitwise AND, bitwise OR and bitwise XOR. Using the example above:
+
+// bitwiseAND(6, 23) ➞ 00000110
+
+// bitwiseOR(6, 23) ➞ 00010111
+
+// bitwiseXOR(6, 23) ➞ 00010001
+// Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+// Examples
+// bitwiseAND(7, 12) ➞ 4
+
+// bitwiseOR(7, 12) ➞ 15
+
+// bitwiseXOR(7, 12) ➞ 11
+// Notes
+// JavaScript has a useful function: toString(2), where you can see the binary representation of a decimal number.
+
+
+// Q-4
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+// Notes
+// Expect any positive number between 1 and 1000.
