6e6bd5c31ec16693349f7c1a0cae597dee22f3fd
dev-salih <salihmu79@gmail.com>
2026-08-15 18:12:17 +0300

    edabit Q-6& Q-7 included

--- Phase 2/edabitPractice/js/script.js
+++ Phase 2/edabitPractice/js/script.js
@@ -148,3 +148,33 @@ console.log(emotify("Make me smile"));
 // addUp(600) ➞ 180300
 // Notes
 // Expect any positive number between 1 and 1000.
+
+
+// Q-6
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+// minMax([2334454, 5]) ➞ [5, 2334454]
+
+// minMax([1]) ➞ [1, 1]
+// Notes
+// All test arrays will have at least one element and are valid.
+
+
+// Q-7
+// Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+
+// Examples
+// timeForMilkAndCookies(new Date(2013, 11, 24)) ➞ true
+
+// timeForMilkAndCookies(new Date(2013, 0, 23)) ➞ false
+
+// timeForMilkAndCookies(new Date(3000, 11, 24)) ➞ true
+// Notes
+// Dates are zero based (see resources).
+// All test cases contain valid dates.\
+
+
+
