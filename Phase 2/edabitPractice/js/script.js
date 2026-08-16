a6fc0a79c5d6ab69ef6f996ecadf459a0f32ef2a
dev-salih <salihmu79@gmail.com>
2026-08-16 07:29:09 +0300

    edabit Q-11 is included

--- Phase 2/edabitPractice/js/script.js
+++ Phase 2/edabitPractice/js/script.js
@@ -253,3 +253,14 @@ findLargestAndSecondLargest([3, 7, 2, 9, 4]);
 // Inspired by a video from Dr. Peter Uelkes.
 // This challenge is describing a factorial.
 
+// Q-11
+// Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+
+// Examples
+// calculator(2, "+", 2) ➞ 4
+
+// calculator(2, "*", 2) ➞ 4
+
+// calculator(4, "/", 2) ➞ 2
+// Notes
+// If the input tries to divide by 0, return: "Can't divide by 0!"
