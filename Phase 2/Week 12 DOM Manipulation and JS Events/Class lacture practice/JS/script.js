ac1bb000b301378d76cb97c79220b273aa0303be
dev-salih <salihmu79@gmail.com>
2026-08-20 22:39:22 +0300

    W-12 selecting ele is done

--- Phase 2/Week 12 DOM Manipulation and JS Events/Class lacture practice/JS/script.js
+++ Phase 2/Week 12 DOM Manipulation and JS Events/Class lacture practice/JS/script.js
@@ -11,3 +11,25 @@ document.body.children[0].innerText = "HELLO WORLD";
         console.log(firstElement);
 
         console.log(firstElement.className);
+        // firstElement.className = "blue";
+      // 1) using getElementById(" ");
+        document.getElementById("four").className ="blue";
+
+      // 2) using querySelector("#___or .___");
+        console.log(document.querySelector("#two"));
+        console.log(document.querySelector(".red"));
+        console.log(document.querySelector("h1"));
+        
+        
+    // b) for multiple ele
+      // 1)  getElementByClassName(".___");
+        let classElements = document.getElementsByClassName("red");
+        console.log(classElements);
+      // 2)  getElementByTagName()
+        let tagElements = document.getElementsByTagName("h1");
+        console.log(tagElements);
+        // console.log(tagElements[1]);
+      // 3)  querySelectorAll("#___ or .__");
+        let queryElements = document.querySelectorAll(".red");
+        console.log(queryElements);
+        // console.log(queryElements[2]);
