86cf0ab436fe50095394caaa1dbae7d3b7df909f
dev-salih <salihmu79@gmail.com>
2026-08-20 17:25:56 +0300

    W-12 DOM class practices is started

--- Phase 2/edabitPractice/js/script.js
+++ Phase 2/edabitPractice/js/script.js
@@ -323,3 +323,31 @@ console.log(paths(9));
 // calculator(4, "/", 2) ➞ 2
 // Notes
 // If the input tries to divide by 0, return: "Can't divide by 0!"
+
+function calculator(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      if (num2 === 0) {
+        return "Cannot divide by 0";
+      }
+      return num1 / num2;
+
+    default:
+      return "Invalid operator";
+  }
+}
+
+console.log(calculator(2, "+", 2));
+console.log(calculator(2, "*", 2));
+console.log(calculator(4, "/", 2));
+console.log(calculator(5, "-", 2));
+console.log(calculator(5, "/", 0));
