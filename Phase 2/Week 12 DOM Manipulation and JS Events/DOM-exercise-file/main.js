82c3ae8b62d2a8c2805376c66a9e15546b3d2ee8
dev-salih <salihmu79@gmail.com>
2026-08-22 14:21:01 +0300

    W-12 Q-1 is done

--- Phase 2/Week 12 DOM Manipulation and JS Events/DOM-exercise-file/main.js
+++ Phase 2/Week 12 DOM Manipulation and JS Events/DOM-exercise-file/main.js
@@ -1,3 +1,10 @@
-//  your solutions start here
-//  make sure to connect your main.js file with your html 
-// happy coding 🧑‍💻
+// Q-1
+  // 1.1
+    let ele = document.getElementById("sample1");
+  // 1.2
+    console.log(ele);
+  // 1.3
+    console.log(ele.textContent);
+
+
+
