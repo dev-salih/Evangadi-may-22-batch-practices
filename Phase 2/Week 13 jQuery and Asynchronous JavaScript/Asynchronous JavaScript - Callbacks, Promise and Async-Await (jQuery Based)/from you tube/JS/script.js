e46a5ab157b6a31be21328d55b3664224996a0d9
dev-salih <salihmu79@gmail.com>
2026-08-30 01:33:44 +0300

    W-13 asynch await new ex is added

--- Phase 2/Week 13 jQuery and Asynchronous JavaScript/Asynchronous JavaScript - Callbacks, Promise and Async-Await (jQuery Based)/from you tube/JS/script.js
+++ Phase 2/Week 13 jQuery and Asynchronous JavaScript/Asynchronous JavaScript - Callbacks, Promise and Async-Await (jQuery Based)/from you tube/JS/script.js
@@ -71,7 +71,30 @@
 
 // All you have to do is write the word async before any regular function and it becomes a promise.
 
+// EX: 
+function toppings_choice() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log("which topping would you love?"));
+    }, 3000);
+  });
+}
 
+async function kitchens() {
+  console.log("A");
+  console.log("B");
+  console.log("C");
+
+  await toppings_choice();
+
+  console.log("D");
+  console.log("E");
+}
+
+kitchens();
+console.log("doing the dishes");
+console.log("cleaning the tables");
+console.log("taking orders");
 
 // ***************************************
 // *                                  
@@ -80,7 +103,11 @@
 // ***************************************
   
 
-  // 1) using <<<< Call Back >>>>
+// #########################################
+// #                                       #
+// #   1) using <<<< Call Back >>>>        #
+// #                                       #
+// ########################################
 
   let stocks = {
     Fruits: ["strawberry", "grapes", "banana", "apple"],
@@ -146,8 +173,11 @@
   // order(0, production);
 
 
-
-  // 2) using <<<< Promise >>>>
+// #########################################
+// #                                       #
+// #    2) using <<<< Promise >>>>         #
+// #                                       #
+// #########################################
   
   // let stocks = {
   //   Fruits: ["strawberry", "grapes", "banana", "apple"],
@@ -156,78 +186,135 @@
   //   toppings: ["chocolate", "peanuts"],
   // };
 
-  let is_shop_open = false;
+  // let is_shop_open = false;
 
-  let order = (time, work) => {
-    return new Promise((resolve, reject) => {
-      if (is_shop_open) {
-        setTimeout(() => {
-          // work is 👇 getting done here
-          resolve(work());
+  // let order = (time, work) => {
+  //   return new Promise((resolve, reject) => {
+  //     if (is_shop_open) {
+  //       setTimeout(() => {
+  //         // work is 👇 getting done here
+  //         resolve(work());
 
-          // Setting 👇 time here for 1 work
-        }, time);
-      } else {
-        reject(console.log("Our shop is closed"));
-      }
-    });
-  };
+  //         // Setting 👇 time here for 1 work
+  //       }, time);
+  //     } else {
+  //       reject(console.log("Our shop is closed"));
+  //     }
+  //   });
+  // };
 
-  // step 1
-  order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
-    // step 2
-    .then(() => {
-      return order(0000, () => console.log("production has started"));
-    })
+  // // step 1
+  // order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+  //   // step 2
+  //   .then(() => {
+  //     return order(0000, () => console.log("production has started"));
+  //   })
 
-    // step 3
-    .then(() => {
-      return order(2000, () => console.log("Fruit has been chopped"));
-    })
+  //   // step 3
+  //   .then(() => {
+  //     return order(2000, () => console.log("Fruit has been chopped"));
+  //   })
 
-    // step 4
-    .then(() => {
-      return order(1000, () =>
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`),
-      );
-    })
+  //   // step 4
+  //   .then(() => {
+  //     return order(1000, () =>
+  //       console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`),
+  //     );
+  //   })
 
-    // step 5
-    .then(() => {
-      return order(1000, () => console.log("start the machine"));
-    })
+  //   // step 5
+  //   .then(() => {
+  //     return order(1000, () => console.log("start the machine"));
+  //   })
 
-    // step 6
-    .then(() => {
-      return order(2000, () =>
-        console.log(`ice cream placed on ${stocks.holder[1]}`),
-      );
-    })
+  //   // step 6
+  //   .then(() => {
+  //     return order(2000, () =>
+  //       console.log(`ice cream placed on ${stocks.holder[1]}`),
+  //     );
+  //   })
 
-    // step 7
-    .then(() => {
-      return order(3000, () =>
-        console.log(`${stocks.toppings[0]} as toppings`),
-      );
-    })
+  //   // step 7
+  //   .then(() => {
+  //     return order(3000, () =>
+  //       console.log(`${stocks.toppings[0]} as toppings`),
+  //     );
+  //   })
 
-    // Step 8
-    .then(() => {
-      return order(2000, () => console.log("Serve Ice Cream"));
-    })
+  //   // Step 8
+  //   .then(() => {
+  //     return order(2000, () => console.log("Serve Ice Cream"));
+  //   })
 
-    // when our original promise is rejected we use .catch()
-    .catch(() => {
-      console.log("Customer left");
-    })
+  //   // when our original promise is rejected we use .catch()
+  //   .catch(() => {
+  //     console.log("Customer left");
+  //   })
 
-    // There's something called the finally handler which works regardless of whether our promise was resolved or rejected.
-    // For example: whether we serve no customers or 100 customers, our shop will close at the end of the day
+  //   // There's something called the finally handler which works regardless of whether our promise was resolved or rejected.
+  //   // For example: whether we serve no customers or 100 customers, our shop will close at the end of the day
 
-    .finally(() => {
-      console.log("end of day");
-    });
+  //   .finally(() => {
+  //     console.log("end of day");
+  //   });
 
+// #########################################
+// #                                       #
+// #     3) using << Asynch Await >>       #
+// #                                       #
+// #########################################
 
-  // 3) using <<<< Asynch Await >>>>
+  // let stocks = {
+  //   Fruits: ["strawberry", "grapes", "banana", "apple"],
+  //   liquid: ["water", "ice"],
+  //   holder: ["cone", "cup", "stick"],
+  //   toppings: ["chocolate", "peanuts"],
+  // };
 
+  // let is_shop_open = true;
+
+    // We're gonna create two functions ->
+      // kitchen: to make ice cream
+      // time: to assign the amount of time each small task will take.
+
+  // function time(ms) {
+  //   return new Promise((resolve, reject) => {
+  //     if (is_shop_open) {
+  //       setTimeout(resolve, ms);
+  //     } else {
+  //       reject(console.log("Shop is closed"));
+  //     }
+  //   });
+  // }
+
+  // async function kitchen() {
+  //   try {
+  //     await time(2000);
+  //     console.log(`${stocks.Fruits[0]} was selected`);
+
+  //     await time(0);
+  //     console.log("production has started");
+
+  //     await time(2000);
+  //     console.log("fruit has been chopped");
+
+  //     await time(1000);
+  //     console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`);
+
+  //     await time(1000);
+  //     console.log("start the machine");
+
+  //     await time(2000);
+  //     console.log(`ice cream placed on ${stocks.holder[1]}`);
+
+  //     await time(3000);
+  //     console.log(`${stocks.toppings[0]} as toppings`);
+
+  //     await time(2000);
+  //     console.log("Serve Ice Cream");
+  //   } catch (error) {
+  //     console.log("customer left");
+  //   }
+  // }
+
+  // kitchen();
