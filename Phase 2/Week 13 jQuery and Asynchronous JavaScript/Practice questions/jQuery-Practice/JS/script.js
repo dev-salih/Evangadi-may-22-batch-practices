0a5458c16168b5dcee9e553549f024c15a2acfd3
dev-salih <salihmu79@gmail.com>
2026-09-01 04:32:53 +0300

    W-13 Q-1 is done

---
+++ Phase 2/Week 13 jQuery and Asynchronous JavaScript/Practice questions/jQuery-Practice/JS/script.js
@@ -0,0 +1,12 @@
+// Q-1
+  // 1.1
+    let sample_1 = $("#sample1");
+    
+  // 1.2
+    console.log("1.1: ", sample_1);
+
+  // 1.3
+    let text = sample_1.text();
+    console.log("1.2: ", text);
+  
+
