5689af3e333eaf2afd618dc22174bd2e22f36f54
dev-salih <salihmu79@gmail.com>
2026-09-01 05:29:53 +0300

    W-13 Q-2 is done

--- Phase 2/Week 13 jQuery and Asynchronous JavaScript/Practice questions/jQuery-Practice/JS/script.js
+++ Phase 2/Week 13 jQuery and Asynchronous JavaScript/Practice questions/jQuery-Practice/JS/script.js
@@ -7,6 +7,37 @@
 
   // 1.3
     let text = sample_1.text();
-    console.log("1.2: ", text);
+    // console.log("1.2: ", text);
   
+// Q-2
+  // 2.1
+    let tech = $("#techCompanies");
+    console.log("2.1: ",tech);
+  
+  // 2.2
+    // let techCount = $("#techCompanies > li").length;
+    let techCount = $("#techCompanies").children("li").length;
+    console.log("2.2: " , techCount);
 
+    // let techCount = $("#techCompanies").find("li").length; we do not use this b/c it is decendent selector
+
+  // 2.3
+    let reds = $(".red");
+    console.log("2.3: ", reds)
+
+  // 2.4
+    let newEle = $("<li>Facebook</li>");
+    console.log("2.4: ", newEle);
+
+  // 2.5 
+    newEle.addClass("blue");
+    console.log("2.5: ", newEle);
+
+  // 2.6 
+    $("li:contains('Sony')").after(newEle);
+
+  // 2.7
+    let blueCount = $("#techCompanies").children(".blue").length;
+    // console.log(blueCount);
+
+    $("#blueCompanies").text(blueCount);
