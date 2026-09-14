b9c048e53de3f2d959a33a045d52bd9efc3df91b
dev-salih <salihmu79@gmail.com>
2026-09-14 23:00:57 +0300

    W-15 Q-1 is done

---
+++ Phase 3/Week 15 Node Modules & Web Servers/Answer for questions/myFirst.js
@@ -0,0 +1,14 @@
+// 1 - (a)
+console.log("My first module");
+
+// 1 - (c)
+function myMultiplier(x) {
+    return 2 * x;
+};
+
+// 1 (D & E)
+let y = myMultiplier(4);
+console.log(y);
+
+// 1 (F)
+module.exports.myMultiplier = myMultiplier;
