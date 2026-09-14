5b5e91c65ef0c97102e39fa2797362c8dc513461
dev-salih <salihmu79@gmail.com>
2026-09-14 23:09:10 +0300

    W-15 Q-2 is done

---
+++ Phase 3/Week 15 Node Modules & Web Servers/Answer for questions/mySecond.js
@@ -0,0 +1,11 @@
+// 2 (a)
+function myMultiplier(num) {
+    return 3 * num;
+}
+
+//  2 (b&c)
+let result = myMultiplier(4);
+console.log(result);
+
+// 2(d)
+module.exports.myMultiplier = myMultiplier;
