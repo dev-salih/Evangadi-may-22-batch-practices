2375f90766a51172ef6191bf44ad6e7e5f166326
dev-salih <salihmu79@gmail.com>
2026-09-26 14:14:47 +0300

    W-16 project- db and server connection are completed

--- Phase 3/Week 16 Working with Databases - MySql/practice from red class/backend/app.js
+++ Phase 3/Week 16 Working with Databases - MySql/practice from red class/backend/app.js
@@ -1,1 +1,38 @@
-console.log("test2");
+// step 1 (import modules)
+const express = require("express");
+const mysql = require("mysql2");
+
+// step 2 (initialize express)
+let app = express();
+
+// step 4 (set up db connection)
+let myDBconnection = mysql.createConnection({
+  user: "crudpracticeuser",
+  password: "crudpracticeuser@1234",
+  host: "localhost",
+  database:"crudpracticeDB",
+});
+
+// step 5 (set up db and user in phpMyAdmin)
+
+// step 6 (connect with db)
+// myDBconnection.connect((err)=>{
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("db connected successfully")
+//     }
+//   }
+// )
+
+// step 3 (create server)
+// let PORT = 4678;
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`server is listening to port ${PORT}`);
+//   }
+// });
+
+// ! better way of db and server connection (step 6 & 3 combined)
